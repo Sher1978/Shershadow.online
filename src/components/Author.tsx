a153ca9366e3352b5791f316f77fa53fd7da489c
Igor Sherlock <sherlock@sarafun.site>
2026-04-07 17:56:37 +0700

    Set Russian as default language and finalize Author video styling

--- src/components/Author.tsx
+++ src/components/Author.tsx
@@ -27,7 +27,7 @@ export default function Author() {
         <div className="grid lg:grid-cols-2 gap-12 md:gap-16 items-center">
           
           {/* Video / Visual Side */}
-          <div className="relative group rounded-sm overflow-hidden border border-white/10 shadow-2xl bg-black aspect-[9/16] max-w-sm mx-auto lg:mx-0">
+          <div className="relative group rounded-sm overflow-hidden border border-white/10 shadow-2xl bg-black aspect-[9/16] w-full max-w-[320px] xs:max-w-[360px] sm:max-w-sm mx-auto lg:mx-0">
             <motion.div
               className="w-full h-full relative"
               initial={{ filter: "grayscale(100%)", opacity: 0.4 }}
@@ -36,10 +36,10 @@ export default function Author() {
               transition={{ duration: 1.2 }}
             >
               <iframe
-                src="https://www.youtube.com/embed/Et9q9fs8p0E?autoplay=1&mute=0&loop=1&playlist=Et9q9fs8p0E&controls=1&showinfo=0&rel=0&iv_load_policy=3&modestbranding=1"
+                src="https://www.youtube.com/embed/Et9q9fs8p0E?autoplay=1&mute=1&loop=1&playlist=Et9q9fs8p0E&controls=0&showinfo=0&rel=0&iv_load_policy=3&modestbranding=1&disablekb=1&fs=0"
                 allow="autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-                className="w-full h-full"
+                allowFullScreen={false}
+                className="w-full h-full pointer-events-none"
                 style={{ border: 'none' }}
               ></iframe>
             </motion.div>
--- src/middleware.ts
+++ src/middleware.ts
@@ -4,19 +4,8 @@ import { i18n } from './i18n-config'
 import { match as matchLocale } from '@formatjs/intl-localematcher'
 import Negotiator from 'negotiator'
 
-function getLocale(request: NextRequest): string | undefined {
-  const negotiatorHeaders: Record<string, string> = {}
-  request.headers.forEach((value, key) => (negotiatorHeaders[key] = value))
-
-  // @ts-ignore locales are readonly
-  const locales: string[] = i18n.locales
-  let languages = new Negotiator({ headers: negotiatorHeaders }).languages(
-    locales
-  )
-
-  const locale = matchLocale(languages, locales, i18n.defaultLocale)
-
-  return locale
+function getLocale(request: NextRequest): string {
+  return i18n.defaultLocale;
 }
 
 export default function proxy(request: NextRequest) {
